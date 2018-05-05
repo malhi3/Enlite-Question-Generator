2e8a8be39d99062666741a990d460787b3a6f4fe
malhi3 <malharthebestfriend@gmail.com>
2018-05-05 14:26:59 +0800

    Added answer key code into javascript, added answer buttons

--- question-generator.js
+++ question-generator.js
@@ -1,4 +1,5 @@
 var storageRef = firebase.storage().ref();
+var ansDatabase = firebase.database();
 
 function randomiseQuestion(){
 	var table = document.getElementById("table");
@@ -32,6 +33,10 @@ function randomiseQuestion(){
 }
 
 function addQuestion(increment, questionNo, parentTdId, parentTrId, incrementTdId, subject){
+	//setting display of answers buttons to block
+	var answersList = document.getElementById("answers-list");
+	answersList.style.display = "block";
+	
 	//Creating table and child tr
 		var table = document.getElementById("table");
 		var tr = document.createElement("tr");
@@ -50,18 +55,47 @@ function addQuestion(increment, questionNo, parentTdId, parentTrId, incrementTdI
 		//Creating question images
 		var question = document.createElement("img");
 		question.setAttribute("id", "Bio"+increment);
-	
+		
+		//Creating numbering on the side
 		var h3 = document.createElement("h3");
-		h3.style.margin = "2px 2px";
-		h3.style.textAlign = "right";
 		h3.innerHTML = increment+".";
+	
+	//Creating Answer Text
+	var ans = document.createElement('h3');
+	ans.setAttribute("class", "answer");
+	ans.style.display = "none";
 	
 	storageRef.child('Paper2/'+subject+'/'+subject.slice(0, 4)+questionNo+'.png').getDownloadURL().then(function(url){
 		//window.alert("function");
 		//window.alert(questionNo);
+		ansDatabase.ref(subject+"/"+subject.slice(0, 4)+questionNo).on('value', function(snapshot){
+			//window.alert(snapshot.val()+" "+questionNo);
+			ans.innerHTML = snapshot.val();
+		});
+		
 		question.setAttribute("src", url);
 	});
 	
+	
 	td.appendChild(question);
-		itd.appendChild(h3);
+	itd.appendChild(h3);
+	itd.appendChild(ans);
+}
+
+function showAnswers(){
+	var answerElements = document.getElementsByClassName('answer');
+	for (var i in answerElements) {
+  		if (answerElements.hasOwnProperty(i)) {
+    		answerElements[i].style.display = 'block';
+  		}
+	}
+}
+
+function hideAnswers(){
+	var answerElements = document.getElementsByClassName('answer');
+	for (var i in answerElements) {
+  		if (answerElements.hasOwnProperty(i)) {
+    		answerElements[i].style.display = 'none';
+  		}
+	}
 }
